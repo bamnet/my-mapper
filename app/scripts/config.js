8486c7e1430a554d7908daec0d6030b6cab93761
Brian Michalski <bmichalski@gmail.com>
2014-05-20 00:47:46 -0400

    Remove the drive.readonly scope, I don't want to have this.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,8 +2,7 @@ var DEVELOPER_KEY = 'AIzaSyBUxDKdJ2UZhshCuq-7c1DVwJZV5U8meDQ';
 var CLIENT_ID = '654605676598-nthfavtrke949shfc49fsn46tdejno7b.apps.googleusercontent.com';
 var SCOPES = [
   'https://www.googleapis.com/auth/drive.file',  // Used to read files opened from within Google Drive.
-  'https://www.googleapis.com/auth/drive.install',  // Open files from within Google Drive.
-  'https://www.googleapis.com/auth/drive.readonly'  // Used to read files picked from the Google Picker.
+  'https://www.googleapis.com/auth/drive.install'  // Open files from within Google Drive.
 ];
 var MIME_TYPES = ['application/json','application/x-javascript','text/javascript',
                   'text/x-javascript','text/x-json','application/octet-stream'];
